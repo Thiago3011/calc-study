8cdffad207b0bb41fc487a8713a7464e776c673e
Thiago3011 <thiago.silva1001@etec.sp.gov.br>
2023-01-09 20:40:50 -0300

    estrutura e começo dos funcionamentos dos botoes

---
+++ script.js
@@ -0,0 +1,27 @@
+const btnsNumbers = document.querySelectorAll('.btns')
+const btnsOperators = document.querySelectorAll('.operators')
+const screen = document.querySelector('.screen')
+
+
+
+function btnListen() {
+
+    btnsNumbers.forEach(btn => {
+        btn.addEventListener('click', () => {
+            screen.value += btn.textContent
+        })
+    })
+
+    btnsOperators.forEach(btn => {
+        btn.addEventListener('click', () => {
+            screen.value += btn.textContent
+
+            if (btn.textContent == 'C') {
+                screen.value = ''
+            }
+        })
+    })
+    
+}
+
+btnListen()
